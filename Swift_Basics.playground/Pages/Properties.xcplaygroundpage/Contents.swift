6249ab68e508407992019ed181c4ae56416433da
Harshvardhan Arora <swiftsimplified@gmail.com>
2022-05-27 10:29:02 +0530

    Properties Part 2

--- Swift_Basics.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Swift_Basics.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -24,4 +24,85 @@ springVacation.pictures.append(contentsOf: ["eiffeltower.jpg", "sienne.jpg"])
 print(springVacation.pictures)
 // Output - ["louvre.jpg", "eiffeltower.jpg", "sienne.jpg"]
 
-print("That's it for the day!")
+/*
+ -----------------------------------------------------------------------------------------------------
+ |  Lazy Properties in Swift
+ -----------------------------------------------------------------------------------------------------
+*/
+
+class Lazy {
+    var normalProperty: String = {
+        let message = "This is a normal property"
+        print(message)
+        return message
+    }()
+    lazy var lazyProperty: String = {
+        let message = "This is a lazy property"
+        print(message)
+        return message
+    }()
+}
+
+let lazyClass = Lazy()
+// Output - This is a normal property
+
+print(lazyClass.lazyProperty)
+/* Output -
+This is a lazy property
+This is a lazy property
+*/
+
+/*
+ -----------------------------------------------------------------------------------------------------
+ |  Computed Properties in Swift
+ -----------------------------------------------------------------------------------------------------
+*/
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    
+    var fullName: String {
+        firstName + " " + lastName
+    }
+}
+
+var mark = Person(firstName: "Mark", lastName: "Dawson")
+print(mark.fullName)
+// Output - Mark Dawson
+
+mark.lastName = "Turner"
+print(mark.fullName)
+// Output - Mark Turner
+
+
+let abcd = "That's it for the day!"
+
+struct Student {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    
+    var fullName: String {
+        get {
+            return firstName + " " + lastName
+        }
+        set(newName) {
+            let split = newName.split(separator: " ")
+            firstName = String(split[0])
+            lastName = String(split[1])
+        }
+    }
+    
+    var description: String {
+        fullName + " is a student of age \(age)"
+    }
+}
+
+var student = Student(firstName: "Harry", lastName: "Dawson", age: 15)
+print(student.fullName)
+// Output - Harry Dawson
+
+student.fullName = "Harry Potter"
+print(student.lastName)
+// Output - Potter
